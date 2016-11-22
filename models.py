d4857e9b3ff853966619f2799d746cf070bf82de
Adrien <adrien@arccoza.com>
2016-11-22 23:36:09 +0900

    Small tweaks and tidy up to markup, css, and folders.

--- models.py
+++ models.py
@@ -5,9 +5,11 @@ movies = [
   Movie(title='The Fifth Element'),
   Movie(title='The Dirty Dozen'),
   Movie(title='Escape from New York'),
+  Movie(title='Serenity'),
   Movie(title='Kill Bill Vol. 1'),
   Movie(title='Kill Bill Vol. 2'),
-  Movie(title='Cowboy Bebop')
+  Movie(title='Cowboy Bebop'),
+  Movie(title='Porco Rosso')
 ]
 
 for m in movies:
